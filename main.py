b125e3fbac498dad962d252cdeacd333a8eba57a
DarshanKudrigi <darshankudrigi2005@gmail.com>
2026-07-23 01:20:08 +0530

    Dynamic_routes_in_FastAPI

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+from numpy import number
 
-app = FastAPI()
+app = FastAPI();
+
+darshan = "Hello, FastAPI!";
+suman = 42;
 
 @app.get("/")
 def home():
-    return {"message": "Hello, FastAPI!"}
+    return {"darshan": darshan,
+            "ragu": suman};
 
-@app.get("/about")
-def about():
-    return {"message": "This is my first FastAPI application."}
+@app.get("/about/{user_id}")
+def about(user_id: int):
+    return {"message": "This is my first FastAPI application.", "user_id": user_id};
 
+@app.get("/square/{number}")
+def square(number: str):
+    return {"square": int(number) ** 2}
