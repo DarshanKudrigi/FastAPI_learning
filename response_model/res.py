c6e088647682986158f6e0c2d68f3c928a73ff95
DarshanKudrigi <darshankudrigi2005@gmail.com>
2026-08-02 13:06:15 +0530

    Response_model_and_Status_Codes

--- response_model/res.py
+++ response_model/res.py
@@ -16,10 +16,36 @@ def users():
             "id":1,
             "name":"Ram",
             "password":"123"
-        },
+        }, 
         {
             "id":2,
             "name":"Shyam",
             "password":"456"
         }
     ]
+
+
+
+# 1. What is a Response Model?
+
+# Answer: A response model defines the structure of the data an API returns. It validates the output, filters extra fields, and ensures a consistent response format.
+# ✅ Data validation
+# ✅ Removes unwanted fields
+# ✅ Hides passwords and sensitive data
+
+
+# 2. What happens if the returned data doesn't match the response model?
+
+# Answer: FastAPI validates the response. If required fields are missing or have incorrect types, it raises a validation error instead of returning invalid data.
+
+
+
+
+# "A response model in FastAPI is a Pydantic model that defines the structure of the API's output. It validates the response, removes unwanted or sensitive fields such as passwords, ensures a consistent JSON format, and helps generate accurate API documentation automatically."
+
+
+
+# BaseModel:: is used to define the schema (the shape of the data).
+
+
+# response_model:: tells FastAPI which schema to use when formatting and validating the API response.
---
+++ status_code__Response/status_code.py
@@ -0,0 +1,38 @@
+# from fastapi import FastAPI, status
+
+# app = FastAPI()
+
+# @app.post("/user", status_code=status.HTTP_201_CREATED)
+# def create_user():
+
+#     return {
+#         "message": "User Created Successfully"
+#     }
+
+
+#Status Code : 201 Created
+
+
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+# from fastapi import FastAPI, status
+
+# app = FastAPI()
+
+# @app.delete("/user/{id}",status_code=status.HTTP_204_NO_CONTENT)
+# def delete_user(id: int):
+#     return
+
+
+# # Output
+
+# Status code : 204 No Content
+
+
+
+
+
+
